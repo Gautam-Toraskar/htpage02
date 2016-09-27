9f7bf2b216e38c6af0166a143851908d6ca3d4e0
Gautam Toraskar <gtmtoraskar19@gmail.com>
2016-09-27 19:07:57 +0530

    desktop menu has been completed

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -1,5 +1,7 @@
-$(function(){
-  $(window).resize(function(event) {
+
+(function(){
+
+	var setBannerSize = function(event) {
     var screenHeight = window.innerHeight;
     console.log(screenHeight);
     var divHeight = $(".wrapperBanner").outerHeight();
@@ -7,5 +9,35 @@ $(function(){
     if(screenHeight > divHeight){
       $(".wrapperBanner").outerHeight(screenHeight);
     }
+  };
+  $(window).load(setBannerSize);
+  $(window).resize(setBannerSize);
+
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
   });
-})( jQuery, window, document );
+
+
+  var $navbar = $("header"),
+	    y_pos = $navbar.offset().top + 400,
+	    height = $navbar.height(),
+	    str = "";
+
+	    y_pos = 400;
+  $(document).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop > y_pos + height) {
+      $navbar.removeClass('remove');
+      str = "greater";
+    } else if (scrollTop <= y_pos) {
+      $navbar.addClass('remove');
+      str = "lower";
+    }
+    $(".count").text(str + ": " + scrollTop);
+  });
+
+
+})();
