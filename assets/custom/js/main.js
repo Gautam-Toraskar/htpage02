d270f92d5a172924c62ee79b506c3c7f7dd0c7a4
Gautam Toraskar <gtmtoraskar19@gmail.com>
2016-09-28 18:52:28 +0530

    following issues were resolved: 1. makover is completed and responsive. 2. Swift Grow is corrected. 3. menu is changed.

--- assets/custom/js/main.js
+++ assets/custom/js/main.js
@@ -29,14 +29,14 @@
 
 	var checkScroll = function(event) {
     var scrollTop = $(this).scrollTop();
-    console.log(event.data);
+    // console.log(event.data);
     checkHeight = event.data == undefined ? false : event.data.checkHeight;
     if (!checkHeight) {
     	height = 0;
     } else {
     	height = $navbar.height();
     }
-    console.log(y_pos);
+    // console.log(y_pos);
     if (scrollTop > y_pos + height) {
       $navbar.removeClass('remove');
       str = "greater";
@@ -72,5 +72,44 @@
   		$(this).find('.mobile-sub-menu').eq(0).removeClass('on').addClass('off');
   		$(this).find('.plus').removeClass('rotate');
   	}
+  });
+
+  $(".app-maker-details button.close").on('click', function() {
+    $(this).parent().hide();
+    // $(this).parent().parent().siblings('.short-desc').show();
+
+    if($(window).width() > 768) {
+    	$(this).parent().siblings('.short-desc').show();
+    }
+
+    $(this).parent().prev().show();
+
+    var $appSetupWrapper = $(this).parent().prev();
+    // $appSetupWrapper.css('background-color', 'red');
+
+    $appSetupWrapper.find("h3").removeClass('hide');
+    $appSetupWrapper.find("p").removeClass('hide');
+    $appSetupWrapper.find("button.learn-more-btn").removeClass('hide');
+    // console.log($(this).parent().parent().siblings('.short-desc'));
+  });
+  $(".app-setup-wrapper button.learn-more-btn").on('click', function (event) {
+  	var that = this;
+
+  	if($(window).width() > 768) {
+  		$(".about-app-setup .short-desc").hide();
+    	$(that).parent().show();
+  	} else {
+  		$(that).parent().hide();
+  	}
+
+    var $appSetupWrapper = $(this).closest('.app-setup-wrapper');
+
+    console.log($appSetupWrapper.length);
+    // $appSetupWrapper.hide();
+    $appSetupWrapper.find("h3").addClass('hide');
+    $appSetupWrapper.find("p").addClass('hide');
+    $appSetupWrapper.find("button.learn-more-btn").addClass('hide');
+
+    $appSetupWrapper.next().show();
   });
 })();
